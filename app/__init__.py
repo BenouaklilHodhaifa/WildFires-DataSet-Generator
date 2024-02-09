e71c724856b0ef4ee79f438f1fbe3e28ae690f16
NassimAm <nassimam2003@gmail.com>
2024-02-09 20:43:10 +0100

    Added MySQL Database

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from flask_cors import CORS
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
 
 def create_app():
     load_dotenv()
 
     app = Flask("WildFires_Dataset_Generator")  # Naming our application
-    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
     app.config['TEMP_OPEN_METEO_FOLDER'] = 'app/files/open_meteo'
+    app.config['GFED_FILES_FOLDER'] = 'app/files/gfed'
     # app.config['PAGINATION_PER_PAGE'] = 5
     CORS(app)
     
-    # db.init_app(app)
+    db.init_app(app)
 
     from app.services.datasets.routes import bp as datasets_bp
     app.register_blueprint(datasets_bp, url_prefix="/datasets")
--- app/services/datasets/controllers.py
+++ app/services/datasets/controllers.py
@@ -1,6 +1,24 @@
+from flask import request
+from datetime import datetime as dt
+from .utils.gfed_utils import get_gfed_data_for_range
+import pandas as pd
+
 def populate():
     """Admin route controller for populating the data from sources"""
-    pass
+    start_date = dt.fromisoformat(request.args.get("start_date"))
+    end_date = dt.fromisoformat(request.args.get("start_date"))
+    lng_min = request.args.get("lng_min")
+    lng_max = request.args.get("lng_max")
+    lat_min = request.args.get("lat_min")
+    lat_max = request.args.get("lng_max")
+
+    # Get GFED Data
+    geo_pos_df, time_df, gfed_df = get_gfed_data_for_range(start_date, end_date, lat_min, lat_max,
+                                                           lng_min, lng_max)
+
+    # Get OpenMeteo Data
+    open_meteo_df:pd.DataFrame = None
+
 
 def query():
     """User route controller for getting requested data"""
