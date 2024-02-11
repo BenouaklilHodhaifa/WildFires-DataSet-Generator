a168677f32083503ead672382be4c559062d2caa
NassimAm <nassimam2003@gmail.com>
2024-02-11 20:06:31 +0100

    Added interactions to front

--- app/services/main/routes.py
+++ app/services/main/routes.py
@@ -1,8 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
+import app.services.datasets.controllers as datasets_controllers
+import json
 
 bp = Blueprint("main", __name__)
 
 # Main route for web page home
 @bp.get("/")
 def home_route():
-    return render_template('base.html')
+    limits, status = datasets_controllers.limits()
+    limits = json.loads(limits.data)
+    if status == 200:
+        return render_template('base.html', limits=limits)
+    else:
+        return jsonify({
+            "message": "Server Error",
+            "error": "Couldn't get map limits"
+        }), 500
