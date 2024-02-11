b4a20b70c54fa35992c235384e3dbe343abc4a33
NassimAm <nassimam2003@gmail.com>
2024-02-11 09:39:36 +0100

    Updated user queries

--- app/services/datasets/controllers.py
+++ app/services/datasets/controllers.py
@@ -1,19 +1,39 @@
 from flask import request
 from datetime import date
 from app import db
-from flask import jsonify, make_response, current_app as app
+from flask import jsonify, make_response
 from sqlalchemy import text
 import pandas as pd
 
 def query():
     """User route controller for getting requested data"""
     try:
+        lat_min = float(request.args['lat_min'])
+        lat_max = float(request.args['lat_max'])
+        lng_min = float(request.args['lng_min'])
+        lng_max = float(request.args['lng_max'])
+        start_date = date.fromisoformat(request.args['start_date'])
+        end_date = date.fromisoformat(request.args['end_date'])
+
         connection = db.engine.connect() # Connect to database
-        df = pd.read_sql("SELECT * FROM wildfires_data", connection) # Query data
+        print((
+            "SELECT * FROM wildfires_data "
+            f"WHERE (latitude BETWEEN {lat_min} AND {lat_max}) "
+            f"AND (longitude BETWEEN {lng_min} AND {lng_max}) "
+            f"AND (date BETWEEN {start_date.year}-{str(start_date.month).zfill(2)}-{str(start_date.day).zfill(2)} "
+            f"AND {end_date.year}-{str(end_date.month).zfill(2)}-{str(end_date.day).zfill(2)})"
+        ))
+        df = pd.read_sql(text((
+            "SELECT * FROM wildfires_data "
+            f"WHERE (latitude BETWEEN {lat_min} AND {lat_max}) "
+            f"AND (longitude BETWEEN {lng_min} AND {lng_max}) "
+            f"AND (date BETWEEN '{start_date.year}-{str(start_date.month).zfill(2)}-{str(start_date.day).zfill(2)}' "
+            f"AND '{end_date.year}-{str(end_date.month).zfill(2)}-{str(end_date.day).zfill(2)}')"
+        )), connection) # Query data
 
         # Serve data read as a csv file
-        resp = make_response(df.to_csv())
-        resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
+        resp = make_response(df.to_csv(index=False))
+        resp.headers["Content-Disposition"] = "attachment; filename=data.csv"
         resp.headers["Content-Type"] = "text/csv"
 
         return resp
@@ -30,15 +50,19 @@ def limits():
         # Connect to database
         connection = db.engine.connect()
         # Get limits of data
-        res = connection.execute("SELECT MIN(latitude), MAX(latitude), MIN(longitude), MAX(longitude), MIN(date), MAX(date) from wildfires_data")
-        return jsonify({
-            "lat_min": res[0],
-            "lat_max": res[1],
-            "lng_min": res[2],
-            "lng_max": res[3],
-            "date_min": res[4],
-            "date_max": res[5]
-        }), 200
+        cursor = connection.execute(text("SELECT MIN(latitude), MAX(latitude), MIN(longitude), MAX(longitude), MIN(date), MAX(date) from wildfires_data"))
+        res = {}
+        for r in cursor:
+            res = {
+            "lat_min": r[0],
+            "lat_max": r[1],
+            "lng_min": r[2],
+            "lng_max": r[3],
+            "date_min": r[4],
+            "date_max": r[5]
+        }
+        
+        return jsonify(res), 200
     except Exception as e:
         return jsonify({
             "message": "Server Error",
