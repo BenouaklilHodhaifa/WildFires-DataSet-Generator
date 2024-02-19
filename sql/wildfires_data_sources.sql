f572ec7bf343eb6a9d3b2ebac7372816f886e07e
NassimAm <nassimam2003@gmail.com>
2024-02-19 23:23:59 +0100

    Updated details

--- sql/wildfires_data_sources.sql
+++ sql/wildfires_data_sources.sql
@@ -1,4 +1,4 @@
-SELECT d.name, d.nbr_rows, d.fwi_backtrack_size, d.created_at, COUNT(*)*100/d.nbr_rows ratio FROM wildfires_data w, datasets d WHERE 
+SELECT d.id, d.name, d.nbr_rows, d.fwi_backtrack_size, d.created_at, COUNT(*)*100/d.nbr_rows ratio FROM wildfires_data w, datasets d WHERE 
 (latitude BETWEEN :lat_min AND :lat_max) 
 AND (longitude BETWEEN :lng_min AND :lng_max) 
 AND (date BETWEEN :start_date AND :end_date)
